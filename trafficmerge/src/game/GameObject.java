2abed3e60b5b81e350ac275843644a7c8552d00b
JohBeck <johannes_beck@arcor.de>
2017-06-16 15:48:10 +0200

    - first Signs

--- trafficmerge/src/game/GameObject.java
+++ trafficmerge/src/game/GameObject.java
@@ -15,6 +15,11 @@ public abstract class GameObject {
 		this.image = image;
 	}
 	
+	public GameObject(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
 
 	public abstract void draw(Graphics g);
 	
