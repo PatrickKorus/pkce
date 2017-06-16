f225cdb80b6661a619c2bd80d9e2dbe2feeddaea
PatrickKorus <patrick.korus@gmx.net>
2017-06-16 14:34:40 +0200

    - classstructure

---
+++ trafficmerge/src/game/GameObject.java
@@ -0,0 +1,43 @@
+package game;
+
+import org.newdawn.slick.*;
+
+public abstract class GameObject {
+
+	protected int x;
+	protected int y;
+	protected Image image;
+	
+	public GameObject(int x, int y, Image image) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.image = image;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public Image getImage() {
+		return image;
+	}
+
+	public void setImage(Image image) {
+		this.image = image;
+	}
+	
+	
+}
