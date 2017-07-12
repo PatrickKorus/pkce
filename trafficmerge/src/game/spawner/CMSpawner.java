232f22a9e90e1907c121b3757ef3f7d545df8175
JohBeck <johannes_beck@arcor.de>
2017-07-12 11:36:47 +0200

    pre-pull

--- trafficmerge/src/game/spawner/CMSpawner.java
+++ trafficmerge/src/game/spawner/CMSpawner.java
@@ -72,7 +72,6 @@ public class CMSpawner implements EntitySpawner  {
 
 			Car[] lastCars = lastCarOnLane(game);
 			
-			
 			maxSpd[0] = 150.0;
 			maxSpd[1] = 0.75 * maxSpd[0];
 			laneSpd[0] = 170.0;
@@ -103,7 +102,6 @@ public class CMSpawner implements EntitySpawner  {
 					maxSpd[0] = 0;
 					leftSpawnFree = true;
 				}
-					
 				//spawn if enough space and reset timer
 				if(leftSpawnFree){
 					if(rightTime <= 0.75 * rightTTrigger || laneSpd[1] <= 60){
@@ -130,7 +128,6 @@ public class CMSpawner implements EntitySpawner  {
 					phantomCarR.setSpeed(lastCars[1].getCurrentSpeed());
 					double safetyDist = phantomCarR.getSafetyDistance(lastCars[1]);
 					if(actDist >= safetyDist+12){
-						
 						// convert laneSpd to mps for the calculation
 						maxSpd[1] = mpsTOkmh((0.5 * kmhTOmps(laneSpd[1])) 
 								+ Math.sqrt((Math.pow(0.5 * kmhTOmps(laneSpd[1]) , 2)/4)+ 2* phantomCarR.getMaxBreak() * (actDist - 6)));
@@ -144,7 +141,6 @@ public class CMSpawner implements EntitySpawner  {
 					maxSpd[1] = 0;
 					rightSpawnFree = true;
 				}
-				
 				//limit right speed to left speed
 				if(laneSpd[0] <= maxSpd[1]){
 					maxSpd[1] = laneSpd[0];
@@ -152,15 +148,12 @@ public class CMSpawner implements EntitySpawner  {
 						laneSpd[1] = maxSpd[1];
 					}
 				}
-				
-					
 				//spawn if enough space and reset timer
 				if(rightSpawnFree){
 					spawnRandomCar(maxSpd[1], laneSpd[1], true, game);
 					rightSpawnFree = false;
 					rightTime = 0;
 					rightTTrigger = calcTrigger(true);
-	
 				}
 			}
 			else{
@@ -184,7 +177,6 @@ public class CMSpawner implements EntitySpawner  {
 			}
 			trafficDensity = Density;
 			sigma = (1.0/trafficDensity);
-
 		}
 	}
 	
